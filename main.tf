cfb41677b09de4de755687d02ebc23f987b09fa2
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-06-29 06:44:21 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "my-ec2" {
+  ami                    = "ami-04b70fa74e45c3917"
+  instance_type          = "t2.medium"
+  key_name               = "jenkins-sanjula"
+  vpc_security_group_ids = ["sg-0f9c6cfdcb6404612"]
+
+  tags = {
+    Name = "DeployServer"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.my-ec2.public_ip
+}
