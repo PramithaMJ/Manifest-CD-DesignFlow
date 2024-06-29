dfafcd5b6debb895f57d9ee669ab4c72c0e1ab71
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-06-29 07:10:49 +0530

    update

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "my-ec2" {
   ami                    = "ami-04b70fa74e45c3917"
   instance_type          = "t2.medium"
-  key_name               = "jenkins-sanjula"
+  key_name               = "JenkinsServerPFT"
   vpc_security_group_ids = ["sg-0f9c6cfdcb6404612"]
 
   tags = {
